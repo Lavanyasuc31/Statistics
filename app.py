4ea28e9b35436a671ddb23da6d44e1d89067d981
Nisha <knisha987650@gmail.com>
2024-08-10 10:05:42 +0530

    commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title('Distributions')
-st.title('Play with Different Distributions :blue[cool] :sunglasses:')
+st.title('Play with Different Distributions :sunglasses:')
 
 st.image("download.jpeg", caption='Learn Statistics')
 
