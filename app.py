d46e315c19349946a05d19c539b86fcf37248a41
Nisha <knisha987650@gmail.com>
2024-08-13 14:27:41 +0530

    commit

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import streamlit as st
 
-# Center-aligned and colored titles
-st.markdown("<h1 style='text-align: center; color: red;'>Statistics</h1>", unsafe_allow_html=True)
-st.markdown("<h1 style='text-align: center; color: blue;'>Time to dive into the world of stats and let the numbers do the talking! 😎</h1>", unsafe_allow_html=True)
+st.title('Statistics')
+st.title("Let's Play with statistics :sunglasses:")
 
-
-st.image("stats.jpg", caption='Learn Statistics')
+st.image("stats.jpeg", caption='Learn Statistics')
 
