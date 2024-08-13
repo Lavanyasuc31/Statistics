9adcaf1dd8b24af27e7b8b438013107228205970
Nisha <knisha987650@gmail.com>
2024-08-13 14:28:15 +0530

    commit

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.title('Statistics')
 st.title("Let's Play with statistics :sunglasses:")
 
-st.image("stats.jpeg", caption='Learn Statistics')
+st.image("stats.jpg", caption='Learn Statistics')
 
